8170898684d2de59903d01b639e6091b6c10728a
Bilal Hussain <syedbilal.nasa@gmail.com>
2022-07-24 17:36:55 -0400

    heapsort (buggy)

--- src/SortingAlgorithms/SortingAlgorithms.js
+++ src/SortingAlgorithms/SortingAlgorithms.js
@@ -22,7 +22,7 @@ function mergeSort(arr, l, h, animations, temp) {
 // Merge Implementation
 function merge(arr, l, m, h, animations, temp) {
     let i = l;
-    let j = m+1;
+    let j = m + 1;
     let k = l;
 
     while (i <= m && j <= h) {
@@ -59,22 +59,91 @@ function merge(arr, l, m, h, animations, temp) {
     }
 }
 
-// TODO: Heap Sort
+// Heap Sort Class
+class MinHeap {
+
+    constructor() {
+        this.heap = [];
+    }
+
+    parentIndex(index) {
+        return Math.floor((index - 1) / 2);
+    }
+
+    leftChildIndex(index) {
+        return (2 * index + 1);
+    }
+
+    rightChildIndex(index) {
+        return (2 * index + 2);
+    }
+
+    swap(a, b) {
+        let temp = this.heap[a];
+        this.heap[a] = this.heap[b];
+        this.heap[b] = temp;
+    }
+
+    insert(item) {
+        this.heap.push(item);
+        let index = this.heap.length - 1;
+        let parent = this.parentIndex(index);
+        while (this.heap[parent] && this.heap[parent] < this.heap[index]) {
+            this.swap(parent, index);
+            index = this.parentIndex(index);
+            parent = this.parentIndex(index);
+        }
+    }
+
+    delete() {
+        let item = this.heap.shift();
+        this.heap.unshift(this.heap.pop());
+        let index = 0;
+        let leftChild = this.leftChildIndex(index);
+        let rightChild = this.rightChildIndex(index);
+        while (this.heap[leftChild] && this.heap[leftChild] > this.heap[index] || this.heap[rightChild] > this.heap[index]) {
+            let max = leftChild;
+            if (this.heap[rightChild] && this.heap[rightChild] > this.heap[max]) {
+                max = rightChild
+            }
+            this.swap(max, index);
+            index = max;
+            leftChild = this.leftChildIndex(max);
+            rightChild = this.rightChildIndex(max);
+        }
+        return item;
+    }
+}
+
+// Heap Sort Implementation
+export function heapSort(arr) {
+    let sorted = [];
+    let heap = new MinHeap();
+
+    for (let i = 0; i < arr.length; i++) {
+        heap.insert(arr[i]);
+    }
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        sorted[i] = heap.delete();
+    }
+    return sorted;
+}
 
 // TODO: Quick Sort
 
-// TODO: Bubble Sort
+// Bubble Sort
 export function getBubbleSortAnimations(array) {
     const animations = [];
     for (let i = 0; i < array.length - 1; i++) {
         for (let j = 0; j < array.length - 1 - i; j++) {
-            if (array[j] > array[j+1]) {
-                animations.push([j, j+1, 0, 0, 0]);
-                animations.push([j, j+1, 0]);
-                animations.push([j, array[j+1], j+1, array[j]]);
+            if (array[j] > array[j + 1]) {
+                animations.push([j, j + 1, 0, 0, 0]);
+                animations.push([j, j + 1, 0]);
+                animations.push([j, array[j + 1], j + 1, array[j]]);
                 let temp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temp;
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
         }
     }
